a2fa2306cbb16f3ab90c52d2bea6ded48a2f0f64
ddoong10 <ddoong10@naver.com>
2025-03-31 16:32:28 +0900

    DB 연결

--- src/main/java/uos/aloc/scholar/ScholarApplication.java
+++ src/main/java/uos/aloc/scholar/ScholarApplication.java
@@ -2,12 +2,9 @@ package uos.aloc.scholar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
-@SpringBootApplication(
-	exclude = {DataSourceAutoConfiguration.class}		//데이터베이스 로드 안하고 실행하는 부분 - 데이터베이스 추가되면 지우세요!
-)
+@SpringBootApplication
 public class ScholarApplication {
 
 	public static void main(String[] args) {
--- src/main/java/uos/aloc/scholar/chatting/controller/ChatController.java
+++ src/main/java/uos/aloc/scholar/chatting/controller/ChatController.java
@@ -12,7 +12,6 @@ public class ChatController {
     @PostMapping("/chat")
     public ChatResponseDTO chat(@RequestBody ChatRequestDTO chatRequestDTO) {
         ChatResponseDTO chatResponseDTO = new ChatResponseDTO();
-        // 프론트엔드로부터 받은 메시지를 가공하여 응답 메시지를 생성합니다.
         chatResponseDTO.setMessage("서버 응답입니다.");
         return chatResponseDTO;
     }
