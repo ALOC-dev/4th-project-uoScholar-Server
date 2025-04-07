b99c8e0fc153febdc16802f4d36600f9b2f496fa
ddoong10 <ddoong10@naver.com>
2025-04-07 17:30:18 +0900

    annotation insert

--- src/main/java/uos/aloc/scholar/crawler/repository/NoticeRepository.java
+++ src/main/java/uos/aloc/scholar/crawler/repository/NoticeRepository.java
@@ -1,8 +1,11 @@
 package uos.aloc.scholar.crawler.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import uos.aloc.scholar.crawler.entity.Notice;
 
+@Repository
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
     // 게시글번호가 이미 존재하는지 확인하기 위한 메소드
     boolean existsByPostNumber(Integer postNumber);
