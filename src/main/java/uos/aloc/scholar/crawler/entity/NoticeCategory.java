921e6c373158bdc5d3aef949e8fee19bf0f00741
ddoong10 <ddoong10@naver.com>
2025-08-15 00:32:14 +0900

    notice entity edit 모든 단과대학 및 일반, 학사 공지 포함 + summary

---
+++ src/main/java/uos/aloc/scholar/crawler/entity/NoticeCategory.java
@@ -0,0 +1,14 @@
+package uos.aloc.scholar.crawler.entity;
+
+public enum NoticeCategory {
+    GENERAL,                // 일반공지
+    ACADEMIC,               // 학사공지
+    COLLEGE_HUMANITIES,     // 인문대
+    COLLEGE_SOCIAL_SCIENCES,// 정경대
+    COLLEGE_ENGINEERING,    // 공과대
+    COLLEGE_URBAN_SCIENCE,  // 도과대
+    COLLEGE_ARTS_SPORTS,    // 예체대
+    COLLEGE_BUSINESS,       // 경영대
+    COLLEGE_NATURAL_SCIENCES,// 자과대
+    COLLEGE_LIBERAL_CONVERGENCE // 자융대
+}
--- src/main/java/uos/aloc/scholar/crawler/repository/NoticeRepository.java
+++ src/main/java/uos/aloc/scholar/crawler/repository/NoticeRepository.java
@@ -1,12 +1,12 @@
-package uos.aloc.scholar.crawler.repository;
+// package uos.aloc.scholar.crawler.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
 
-import uos.aloc.scholar.crawler.entity.Notice;
+// import uos.aloc.scholar.crawler.entity.Notice;
 
-@Repository
-public interface NoticeRepository extends JpaRepository<Notice, Long> {
-    // 게시글번호가 이미 존재하는지 확인하기 위한 메소드
-    boolean existsByPostNumber(Integer postNumber);
-}
+// @Repository
+// public interface NoticeRepository extends JpaRepository<Notice, Long> {
+//     // 게시글번호가 이미 존재하는지 확인하기 위한 메소드
+//     boolean existsByPostNumber(Integer postNumber);
+// }
